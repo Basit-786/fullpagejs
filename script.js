493704866bee32b2dd496d0be125d4e8fc152fae
Basit-786 <arsajid090@gmail.com>
2024-02-26 22:10:53 +0500

    first comit

---
+++ script.js
@@ -0,0 +1,14 @@
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  navigation: true,
+  scrollHorizontally: true,
+  slidesNavigation: true,
+
+  controlArrows: true,
+  sectionsColor: ["#C0C0C0", "#C0C0C0", "#C0C0C0"],
+
+  navigationTooltips: ["Floors", "Doors", "Windows"],
+  anchors: ["floor", "door", "window"],
+
+  responsiveSlides: true,
+});
